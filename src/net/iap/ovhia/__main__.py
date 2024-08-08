fcea5acf82047d751a7ceec9b4c15d5bc0b415e8
Iacopo Papalini <iacopo@domotz.com>
2024-08-08 12:43:41 +0200

    support for more subdomains than one

--- src/net/iap/ovhia/__main__.py
+++ src/net/iap/ovhia/__main__.py
@@ -12,23 +12,29 @@ def main():
         **conf["ovh"]
     )
 
-    result = client.get("/domain/zone/{zoneName}/record?subDomain={subdomain}&fieldType=A".format(**conf["dns"]))
-    match len(result):
-        case 0:
-            raise RuntimeError("No zones found for {subdomain}.{zoneName}".format(**conf["dns"]))
-        case 1:
-            zone_id = result[0]
-        case _:
-            raise RuntimeError("Too many zones found for {subdomain}.{zoneName}".format(**conf["dns"]))
+    for subdomain in conf["dns"]["subdomains"]:
+        params = {
+            "zoneName": conf["dns"]["zoneName"],
+            "subdomain": subdomain,
+            "ttl": conf["dns"]["ttl"],
+        }
+        result = client.get("/domain/zone/{zoneName}/record?subDomain={subdomain}&fieldType=A".format(**params))
+        match len(result):
+            case 0:
+                raise RuntimeError("No zones found for {subdomain}.{zoneName}".format(**params))
+            case 1:
+                zone_id = result[0]
+            case _:
+                raise RuntimeError("Too many zones found for {subdomain}.{zoneName}".format(**params))
 
-    zone_url = "/domain/zone/{zoneName}/record/{zone_id}".format(**conf["dns"], zone_id=zone_id)
-    target = client.get(zone_url)["target"]
-    current_ip = whatismyip.whatismyipv4()
-    if target != current_ip:
-        print(f"{datetime.now().isoformat()} - {target} != {current_ip}; upgrading")
-        client.put(zone_url, target=current_ip, subDomain=conf["dns"]["subdomain"], ttl=conf["dns"]["ttl"])
-    else:
-        print(f"{datetime.now().isoformat()} - zone correctly points to {current_ip}")
+        zone_url = "/domain/zone/{zoneName}/record/{zone_id}".format(**params, zone_id=zone_id)
+        target = client.get(zone_url)["target"]
+        current_ip = whatismyip.whatismyipv4()
+        if target != current_ip:
+            print(f"{datetime.now().isoformat()} - {target} != {current_ip}; upgrading")
+            client.put(zone_url, target=current_ip, subDomain=subdomain, ttl=params["ttl"])
+        else:
+            print(f"{datetime.now().isoformat()} - zone correctly points to {current_ip}")
 
 
 if __name__ == '__main__':
